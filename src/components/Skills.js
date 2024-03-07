e9c604b903d234daa48c111a2b5220248a90e3cb
StefaniaPaduraru <stefania.paduraru@student.tuiasi.ro>
2024-03-07 12:09:17 +0200

    added-Marquee-for-ticker-in-skills-section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,48 @@
 import React from "react";
 import "./Skills.scss";
 import { Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
-function Skills(){
-    return(
+function Skills({ direction = "right", speed = 60 }) {
+  return (
     <section id="skills">
-        <hr/>
-        <Container fluid className="skills-container">
-            <p className="skills-component">React <img src="./resources/react.png" alt="react"/></p>
-            <p className="skills-component">CSS <img src="./resources/css.png" alt="react"/></p>
-            <p className="skills-component">HTML <img src="./resources/html.png" alt="react"/></p>
-            <p className="skills-component">Bootstrap <img src="./resources/bootstrap.png" alt="react"/></p>
-            <p className="skills-component">JavaScript <img src="./resources/javascript.png" alt="react"/></p>
-            <p className="skills-component">Sass <img src="./resources/sass.png" alt="react"/></p>
-            <p className="skills-component">Figma <img src="./resources/figma.png" alt="react"/></p>
-        </Container>
-        <hr/>
+      <hr />
+      <Container fluid className="skills-container marquee-container">
+        <Marquee direction={direction} speed={speed}>
+          <p className="skills-component">
+            HTML <br />
+            <img src="./resources/html.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            CSS <br />
+            <img src="./resources/css.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            JavaScript <br />
+            <img src="./resources/javascript.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            React <br />
+            <img src="./resources/react.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            Bootstrap <br />
+            <img src="./resources/bootstrap.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            Sass <br />
+            <img src="./resources/sass.png" alt="react" />
+          </p>
+          <p className="skills-component">
+            Figma <br />
+            <img src="./resources/figma.png" alt="react" />
+          </p>
+        </Marquee>
+      </Container>
+
+      <hr />
     </section>
-    );
+  );
 }
+
 export default Skills;
